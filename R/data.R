94a0656cd3083d12050c5b3eb277f01ca1c9fd1d
jbengler <broder.engler@gmail.com>
2020-02-13 22:37:15 +0100

    fix documentation

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Expression data from RNA-Seq study.
+#'
+#' Some more desciption.
+#'
+#' @format A data frame with 800 rows and 12 variables:
+#' \describe{
+#'   \item{ensembl_gene_id}{...}
+#'   \item{external_gene_name}{...}
+#'   \item{Eip_vs_Hip.padj}{...}
+#'   \item{direction}{...}
+#'   \item{is_immune_gene}{...}
+#'   \item{count_type}{...}
+#'   \item{group}{...}
+#'   \item{replicate}{...}
+#'   \item{expression}{...}
+#'   \item{sample}{...}
+#'   \item{sample_type}{...}
+#'   \item{condition}{...}
+#' }
+#' @source <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE104899>
+"data_exprs"
--- R/tidyheatmap.R
+++ R/tidyheatmap.R
@@ -1,4 +1,6 @@
 
+globalVariables(".")
+#' @importFrom dplyr %>%
 bro_string_diff <- function(x) {
   (x[-length(x)] != x[-1]) %>%
     which(.)
@@ -7,41 +9,41 @@ bro_string_diff <- function(x) {
 wrangle_data <- function(df, rows, columns, values, ann_row = NULL, ann_col = NULL, gaps_row = NULL, gaps_col = NULL){
   m <-
     df %>%
-    select({{rows}}, {{columns}}, {{values}}) %>%
-    pivot_wider(names_from = {{columns}}, values_from = {{values}}) %>%
-    column_to_rownames(colnames(.)[1])
+    dplyr::select({{rows}}, {{columns}}, {{values}}) %>%
+    tidyr::pivot_wider(names_from = {{columns}}, values_from = {{values}}) %>%
+    tibble::column_to_rownames(colnames(.)[1])
 
-  if(!rlang::quo_is_null(enquo(ann_col))) {
+  if(!rlang::quo_is_null(rlang::enquo(ann_col))) {
     ann_col <-
       df %>%
-      select({{columns}}, {{ann_col}}) %>%
-      distinct() %>%
-      column_to_rownames(colnames(.)[1])
+      dplyr::select({{columns}}, {{ann_col}}) %>%
+      dplyr::distinct() %>%
+      tibble::column_to_rownames(colnames(.)[1])
   } else ann_col <- NA
 
-  if(!rlang::quo_is_null(enquo(ann_row))) {
+  if(!rlang::quo_is_null(rlang::enquo(ann_row))) {
     ann_row <-
       df %>%
-      select({{rows}}, {{ann_row}}) %>%
-      distinct() %>%
-      column_to_rownames(colnames(.)[1])
+      dplyr::select({{rows}}, {{ann_row}}) %>%
+      dplyr::distinct() %>%
+      tibble::column_to_rownames(colnames(.)[1])
   } else ann_row <- NA
 
-  if(!rlang::quo_is_null(enquo(gaps_row))) {
+  if(!rlang::quo_is_null(rlang::enquo(gaps_row))) {
     gaps_row <-
       df %>%
-      select({{rows}}, {{gaps_row}}) %>%
-      distinct() %>%
-      pull({{gaps_row}}) %>%
+      dplyr::select({{rows}}, {{gaps_row}}) %>%
+      dplyr::distinct() %>%
+      dplyr::pull({{gaps_row}}) %>%
       bro_string_diff()
   }
 
-  if(!rlang::quo_is_null(enquo(gaps_col))) {
+  if(!rlang::quo_is_null(rlang::enquo(gaps_col))) {
     gaps_col <-
       df %>%
-      select({{columns}}, {{gaps_col}}) %>%
-      distinct() %>%
-      pull({{gaps_col}}) %>%
+      dplyr::select({{columns}}, {{gaps_col}}) %>%
+      dplyr::distinct() %>%
+      dplyr::pull({{gaps_col}}) %>%
       bro_string_diff()
   }
 
@@ -58,29 +60,36 @@ wrangle_data <- function(df, rows, columns, values, ann_row = NULL, ann_col = NU
 #' @param rows Column of the dataframe to use for heatmap rows.
 #' @param columns Column of the dataframe to use for heatmap columns.
 #' @param values Column of the dataframe containing the values to plot.
-#' @param ann_row,ann_col Column(s) of the dataframe to use for row and column annotation.
-#' Multiple columns can be provided as vector, e.g. `c(column1, column2, column3)`.
-#' @param gaps_row,gaps_col Column of the dataframe to use for row and column gaps.
 #' @param ann_colors List of colors used to color code row and column annotations.
 #' See `pheatmap::pheatmap()` for details.
-#' @param color_scale_min,color_scale_max Minimal and maximal value on color scale.
 #' @param color_scale_n Number of color on color scale.
 #' @param breaks Sequence used for mapping values to colors. Is one element longer than `color_scale_n`.
 #' See `pheatmap::pheatmap()` for details.
 #' @param scale Character indicating if the values should be centered and scaled by "row", "column" or "none".
 #' Default is "row".
 #' @param fontsize Base fontsize for the plot.
-#' @param cellwidth,cellheight Cell width and hieght in points. If left `NA`, cells will scale to fill the available space.
 #' @param color Vector of colors used in heatmap.
 #' @param return_data If `TRUE` processed data is returned instead of plot.
 #' @param ... Other parameters passed to `pheatmap::pheatmap()`.
+#' @param filename todo
+#' @param ann_row todo
+#' @param ann_col todo
+#' @param gaps_row todo
+#' @param gaps_col todo
+#' @param cellwidth todo
+#' @param cellheight todo
+#' @param color_scale_min todo
+#' @param color_scale_max todo
+#' @param cluster_rows todo
+#' @param cluster_cols todo
+#' @param border_color todo
 #'
 #' @export
 tidy_heatmap <- function(df, rows, columns, values, filename = NA, ann_row = NULL, ann_col = NULL,
                          gaps_row = NULL, gaps_col = NULL, ann_colors = NA, breaks = NA,
                          scale = "none", fontsize = 7, cellwidth = NA, cellheight = NA,
                          color_scale_min = NA, color_scale_max = NA, color_scale_n = 100,
-                         color = colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name ="RdYlBu")))(color_scale_n),
+                         color = grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name ="RdYlBu")))(color_scale_n),
                          return_data = FALSE, cluster_rows = FALSE, cluster_cols = FALSE, border_color = NA, ...) {
 
   heatmap_data <-
