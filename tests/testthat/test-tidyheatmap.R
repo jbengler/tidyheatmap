0fc59b232b340fed117813dbd75a4cb4796e0ef9
jbengler <broder.engler@gmail.com>
2020-02-19 22:37:40 +0100

    add tests

--- tests/testthat/test-tidyheatmap.R
+++ tests/testthat/test-tidyheatmap.R
@@ -18,4 +18,21 @@ test_that("gtable output is produced", {
                             rows = external_gene_name,
                             columns = sample,
                             values = expression)$gtable %>% class %>% .[1], "gtable")
+  expect_equal(tidy_heatmap(data_exprs,
+                            rows = external_gene_name,
+                            columns = sample,
+                            values = expression,
+                            scale = "row",
+                            color_legend_n = 5,
+                            color_legend_min = -1,
+                            color_legend_max = 1,
+                            colors = c("#145afc","#ffffff","#ee4445"))$gtable %>% class %>% .[1], "gtable")
+  expect_equal(tidy_heatmap(data_exprs,
+                            rows = external_gene_name,
+                            columns = sample,
+                            values = expression,
+                            annotation_col = c(sample_type, condition, group),
+                            annotation_row = c(is_immune_gene, direction),
+                            gaps_row = direction,
+                            gaps_col = group)$gtable %>% class %>% .[1], "gtable")
 })
